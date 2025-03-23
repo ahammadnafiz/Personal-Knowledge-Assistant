08d3ea970d836b094ccaf1c583c6fecfd083bff0
habib-153 <h.r.sihab155@gmail.com>
2025-03-23 23:09:48 +0600

    fix: fix the error of tailwind

--- frontend/src/components/ui/textarea.tsx
+++ frontend/src/components/ui/textarea.tsx
@@ -1,6 +1,6 @@
+import { cn } from "@/libs"
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
 
 function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
   return (
